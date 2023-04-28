ec7bb9b90e4deab149c28c5f97a2365f12c43997
Rashad Azimov <azimovrasad@gmail.com>
2023-04-28 12:53:25 +0400

    updating

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataAccess;
+using Entities;
+
+Console.WriteLine("Hello, World!");
+
+createDb();
+
+static void createDb()
+{
+    using(var ctx = new OpsServerContext())
+    {
+        ctx.Database.EnsureCreated();
+
+        ctx.Users.Add(new User { Name = "Rashad", Email = "azimov@gmail.com" }) ;
+        ctx.Users.Add(new User { Name = "Elmir", Email = "elmir@rzayev.com" });
+        ctx.SaveChanges();
+    }
+}
---
+++ DataAccess/OpsServerContext.cs
@@ -0,0 +1,42 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class OpsServerContext : DbContext
+    {
+
+        // for getting ConnectionString from JSON
+       public static String get()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("C:\\Users\\Rashad\\source\\repos\\SocarProject\\WebApi\\appsettings.json");
+            IConfiguration config = builder.Build();
+            string connectionString = config.GetConnectionString("MySqlConnection");
+            return connectionString;
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.Name).IsRequired().HasColumnType("nvarchar(25)");
+                entity.Property(x => x.Email).IsRequired().HasColumnType("nvarchar(25)");
+
+            });
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(OpsServerContext.get());
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
